6bc5364d387ca7d74b128200ed52178703f06a5e
SL3Z4K <slezak.krisztian2004@gmail.com>
2024-05-07 18:50:53 +0200

    foglalas osztály létrehozása

--- szalloda.py
+++ szalloda.py
@@ -17,6 +17,9 @@ class EgyagyasSzoba(Szoba):
     def leiras(self):
         return f"Egyágyas szoba {self.szobaszam}-as sorszámmal. Az ára: {self.ar} Ft. egy éjszakára. Mérete: {self.meret}."
 
+    def __repr__(self):
+        return f"Egyágyas szoba(szobaszám={self.szobaszam}, méret='{self.meret}')"
+
 class KetagyasSzoba(Szoba):
     def __init__(self, szobaszam, meret):
         super().__init__(ar=70000, szobaszam=szobaszam)
@@ -24,6 +27,9 @@ class KetagyasSzoba(Szoba):
 
     def leiras(self):
         return f"Kétágyas szoba {self.szobaszam}-as sorszámmal. Az ára: {self.ar} Ft. egy éjszakára. Mérete: {self.meret}."
+
+    def __repr__(self):
+        return f"Kétágyas szoba(szobaszam={self.szobaszam}, méret='{self.meret}')"
 
 class Szalloda:
     def __init__(self, nev):
@@ -36,10 +42,22 @@ class Szalloda:
     def szobak_szama(self):
         return len(self.szobak)
 
+class Foglalas:
+    def __init__(self, szoba, datum):
+        self.szoba = szoba
+        self.datum = datum
 
+    def __repr__(self):
+        return f"Foglalt szoba: {self.szoba}, dátum:{self.datum}"
 
 szalloda = Szalloda("Isten Király Hotel")
 szalloda.add_szoba(EgyagyasSzoba(101, "Kicsi"))
 szalloda.add_szoba(KetagyasSzoba(102, "Nagy"))
+foglalas1 = Foglalas(EgyagyasSzoba(101, "Kicsi"), "2024-06-23")
+foglalas2 = Foglalas(KetagyasSzoba(102, "Nagy"), "2024-07-25")
+
 
 print(f"{szalloda.nev} szállodában összesen {szalloda.szobak_szama()} szoba van.")
+
+print(foglalas1)
+print(foglalas2)
