22ebdb642ae87173cc946abd055d3b5afa5cc26d
SL3Z4K <slezak.krisztian2004@gmail.com>
2024-05-07 18:23:27 +0200

    absztrakt osztály létrehozása

--- szalloda.py
+++ szalloda.py
@@ -1,1 +1,10 @@
-asdas
+from abc import ABC, abstractmethod
+
+class Szoba(ABC):
+    def __init__(self, ar, szobaszam):
+        self.ar = ar
+        self.szobaszam = szobaszam
+
+        @abstractmethod
+        def leiras(self):
+            pass
