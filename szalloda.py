fd04cb1e931a6be68e502659cce93f1ca8503c56
SL3Z4K <slezak.krisztian2004@gmail.com>
2024-05-07 18:30:12 +0200

    egyagyas es ketagyas szobak

--- szalloda.py
+++ szalloda.py
@@ -8,3 +8,19 @@ class Szoba(ABC):
         @abstractmethod
         def leiras(self):
             pass
+
+class EgyagyasSzoba(Szoba):
+    def __init__(self, szobaszam, meret):
+        super().__init__(ar=45000, szobaszam=szobaszam)
+        self.meret = meret
+
+    def leiras(self):
+        return f"Egyágyas szoba {self.szobaszam}-as sorszámmal. Az ára: {self.ar} Ft. egy éjszakára. Mérete: {self.meret}."
+
+class KetagyasSzoba(Szoba):
+    def __init__(self, szobaszam, meret):
+        super().__init__(ar=70000, szobaszam=szobaszam)
+        self.meret = meret
+
+    def leiras(self):
+        return f"Kétágyas szoba {self.szobaszam}-as sorszámmal. Az ára: {self.ar} Ft. egy éjszakára. Mérete: {self.meret}."
