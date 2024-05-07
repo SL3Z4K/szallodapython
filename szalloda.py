fa51514c5d9ae09806fcf413cb8e1e753a1bfaec
SL3Z4K <slezak.krisztian2004@gmail.com>
2024-05-07 18:38:05 +0200

    szalloda osztály létrehozása majd tesztelés

--- szalloda.py
+++ szalloda.py
@@ -24,3 +24,22 @@ class KetagyasSzoba(Szoba):
 
     def leiras(self):
         return f"Kétágyas szoba {self.szobaszam}-as sorszámmal. Az ára: {self.ar} Ft. egy éjszakára. Mérete: {self.meret}."
+
+class Szalloda:
+    def __init__(self, nev):
+        self.nev = nev
+        self.szobak = []
+
+    def add_szoba(self, szoba):
+        self.szobak.append(szoba)
+
+    def szobak_szama(self):
+        return len(self.szobak)
+
+
+
+szalloda = Szalloda("Isten Király Hotel")
+szalloda.add_szoba(EgyagyasSzoba(101, "Kicsi"))
+szalloda.add_szoba(KetagyasSzoba(102, "Nagy"))
+
+print(f"{szalloda.nev} szállodában összesen {szalloda.szobak_szama()} szoba van.")
